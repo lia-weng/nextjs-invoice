21efe58ead7174ea0364f40b12eb6ffa20dc085b
lia-weng <lia.xin.weng@gmail.com>
2024-01-15 22:28:27 -0500

    chapter 9 streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,6 +37,8 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
 
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -58,6 +60,8 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   noStore();
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
 
   try {
     // You can probably combine these into a single SQL query
